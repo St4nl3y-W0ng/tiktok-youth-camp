9e42972da0527d455b1d13b411ca5cbfd029311d
Stanley Wong <70022847+St4nl3y-W0ng@users.noreply.github.com>
2022-05-23 16:37:53 +0800

    Add files via upload
    Components, no game, no css

---
+++ main/src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Choose from "./choose/Choose";
+import Login from "./login/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Game from "./game/Game";
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <div className="content">
+                    <Routes>
+                        {/* <Route path="/" element={<Login />}>
+                            <Route index element={<Login />} />
+                            <Route path="choose" element={<Choose />}>
+                                <Route index element={<Choose />} />
+                                <Route path="game" element={<Game />} />
+                            </Route>
+                        </Route> */}
+                        <Route path="/" element={<Login />} />
+                        <Route path="choose" element={<Choose />} />
+                        <Route path="game" element={<Game />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
+}
+export default App; 
