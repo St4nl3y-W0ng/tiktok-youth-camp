ff4f19a769b0c2149fd86d1f69d9692734a92ec8
shufeiii <105223219+shufeiii@users.noreply.github.com>
2022-05-25 21:26:06 +0800

    Add files via upload

--- main/src/components/login/Login.jsx
+++ main/src/components/login/Login.jsx
@@ -21,22 +21,60 @@ function Login() {
         })
     }
 
-    return (<div>
-        <h1>Welcome to {login.rounds} {(login.rounds === "1") ? "round" : "rounds"} of Hangman, {login.userName}</h1>
-        <form onSubmit={handleClick}>
-            <Input name="userName"
-                value={login.userName}
-                placeholder="Enter a Username"
-                onChange={handleChange}
-            />
-            <Input name="rounds"
-                value={login.rounds}
-                placeholder="How many rounds of Hangman do you want to play?"
-                onChange={handleChange}
-            />
-            <button type="submit">Login</button>
-        </form>
-    </div>
+    return (
+    
+        <section className="bottom-section" id="features">
+
+        <div className="row">
+  
+          <div className="user-info-card col-lg-6 col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <form onSubmit={handleClick}>
+                  <div className="form-group row">
+                    <label for="num-rounds" className="col-sm-4 col-form-label"><strong>No. of rounds :</strong></label>
+                        <div className="col-sm-6">
+                            <Input name="rounds"
+                            value={login.rounds}
+                            id="num-rounds"
+                            onChange={handleChange}
+                            />
+                        </div>
+                  </div>
+                    <button className="btn btn-lg btn-light btn-outline-dark" type="submit">Next →</button>
+                </form>
+              </div>
+            </div>
+          </div>
+  
+          <div className="instruction-card col-lg-6 col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h4><u>Instructions</u></h4>
+                <p>The Hangman Game is a game where the player attempts to guess the letters of an unknown word. For each incorrect guess, a part of the hanged man will be drawn. The round ends when the full hanged man is drawn. Try your best to guess the word before the full hanged man is drawn!</p>
+                <span id="more">
+                
+                <ol>
+                  <li>Choose your preferred number of rounds you wish to play for. (5, 10, 15)</li>
+                  <li>Select <span id="next">"Next→"</span> and select your preferred theme for the rounds. Do note that you are given a fixed time of 1 minute per round.</li>
+                  <li>Input a single alphabet as your guess. 
+                    <br />If you require a hint, you may select the <span id="help">"Get Help"</span> button. 
+                    <br /> If you opt to give up and skip to the next word, you may select <br /><span id="skip">"Skip to next"</span> button.</li>
+                  <li>Should you opt to exit the game, you may select the <span id="exit">"Exit Game"</span> button.</li>
+                  <li>All the best!</li>
+  
+                </ol>
+              </span>
+              <button onclick="showMore()" id="myBtn">For more instructions</button>
+              
+              </div>
+            </div>
+          </div>
+  
+        </div>
+      </section>
+
+    
     );
 }
 export default Login;
