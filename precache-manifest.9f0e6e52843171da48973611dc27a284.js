f581263c0a566a2d2dc068ce41b79a146554844c
st4nl3y-w0ng <70022847+St4nl3y-W0ng@users.noreply.github.com>
2022-05-29 23:18:12 +0800

    Updates

---
+++ precache-manifest.9f0e6e52843171da48973611dc27a284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75304cae7f112aeb831a48b905c4162",
+    "url": "/tiktok-youth-camp/index.html"
+  },
+  {
+    "revision": "726f456d109a7165eb57",
+    "url": "/tiktok-youth-camp/static/css/2.7020638d.chunk.css"
+  },
+  {
+    "revision": "726f456d109a7165eb57",
+    "url": "/tiktok-youth-camp/static/js/2.692e73d4.chunk.js"
+  },
+  {
+    "revision": "3969dfa86843be71a2aa",
+    "url": "/tiktok-youth-camp/static/js/main.9670edab.chunk.js"
+  },
+  {
+    "revision": "eb37439e699ee2d6108f",
+    "url": "/tiktok-youth-camp/static/js/runtime-main.1df7f083.js"
+  }
+]);
